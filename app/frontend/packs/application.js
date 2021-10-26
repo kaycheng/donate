e48b2dafde2a5499ab16b545781cf03a739f1e17
kaycheng <kaycheng717@gmail.com>
2021-10-26 11:37:59 +0800

    Yarn upgrade and change app/javascript to app/frontend

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "../stylesheets/application"
+import "../javascripts/application"
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
