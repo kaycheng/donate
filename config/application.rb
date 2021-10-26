f425771491631b643db9e9d01c671dbdb7752be3
kaycheng <kaycheng717@gmail.com>
2021-10-26 13:57:08 +0800

    Create welcome page to root page

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module Donate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
