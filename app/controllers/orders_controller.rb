b5b206d11debd621f70ef29ee07c1b0913b30919
kaycheng <kaycheng717@gmail.com>
2021-11-26 13:07:17 +0800

    Add create action and add validation on order's model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,23 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-
+  
   def new
     @order = current_user.orders.new
+  end
+
+  def create
+    @order = current_user.orders.new(order_params)
+    if @order.save
+      # do something
+      redirect_to root_path, notice: "Thank you for donating!"
+    else
+      # do something
+      render :new, flash_now: "There are some errors occured!"
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:username, :amount, :memo, :pay_type)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,20 @@
 class Order < ApplicationRecord
   belongs_to :user
+  
+  validates :username, presence: true
+  validates :amount, numericality: { greater_than: 0 }
+  validates :order_no, uniqueness: true 
+  
+  before_validation :build_order_no, on: :create
 
   enum pay_type: {
     "ATM": "A",
     "Credit Card": "C"
   }
+  
+  private 
+  def build_order_no
+    now = Date.current
+    self.order_no = "RB#{now.strftime('%Y%m%d')}#{[*'A'..'Z', *'0'..'9'].sample(10).join}"
+  end
 end
